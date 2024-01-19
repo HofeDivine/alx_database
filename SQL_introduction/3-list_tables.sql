510f252697213a0feea727c763668976d978f038
HofeDivine <hofedive@gmail.com>
2024-01-19 11:21:58 -1200

    COMMIT

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,6 @@
 -- A script that lists all the tables of a database in your MySQL server.
-SET @dbname = ?; 
+SET database_nane = ?; 
+USE database_nane;
 
-SET @query = CONCAT('
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = ''', @dbname, ''';
+SET @query = CONTACT('SHOW TABLES;
 ');
-
--- Prepare and execute the query
-PREPARE stmt FROM @query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
