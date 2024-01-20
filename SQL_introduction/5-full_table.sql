a9f2c9992998c86843cf984c22590c7904ab0092
HofeDivine <hofedive@gmail.com>
2024-01-20 10:59:19 -1200

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,6 @@
 -- Describe a table
 USE hbtn_0c_0;
-SHOW COLUMNS FROM first_table;
+
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
