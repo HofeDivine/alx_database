720e4679d50c23e7361fce30d45249e3531cfc0e
HofeDivine <hofedive@gmail.com>
2024-01-19 14:22:19 -1200

    c ommit

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,12 +3,17 @@ DELIMITER //
 
 CREATE PROCEDURE CreateTable(IN dbName VARCHAR(255))
 BEGIN
-    SET @query = CONCAT('
-        CREATE TABLE IF NOT EXISTS ', dbName, '.first_table (
+    SET @query = CONCAT('USE ', dbName, ';');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    SET @query = '
+        CREATE TABLE IF NOT EXISTS first_table (
             id INT,
             name VARCHAR(256)
         );
-    ');
+    ';
     PREPARE stmt FROM @query;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
