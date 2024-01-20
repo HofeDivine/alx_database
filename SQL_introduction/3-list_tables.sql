571444f14a7d2cdaeead69b44ba36b13ab74c489
HofeDivine <hofedive@gmail.com>
2024-01-19 14:12:54 -1200

    commit

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,10 +1,16 @@
 -- A script that lists all the tables of a database in your MySQL server.
 -- list_tables_script.sql
-SET @database_name = ?;  -- Placeholder for the database name
+DELIMITER //
 
-SELECT TABLE_NAME
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = @database_name;
+CREATE PROCEDURE ListTables(IN dbName VARCHAR(255))
+BEGIN
+    SET @query = CONCAT('SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = ''', dbName, ''';');
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END //
+
+DELIMITER ;
 
 
 
