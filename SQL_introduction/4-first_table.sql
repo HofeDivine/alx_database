3a6dfb00eb9e2c4c42a5a51d836710b17fb1add1
HofeDivine <hofedive@gmail.com>
2024-01-20 10:35:43 -1200

    COMMIT

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,22 +1,6 @@
 -- create_table_script.sql
-DELIMITER //
-
-CREATE PROCEDURE CreateTable(IN dbName VARCHAR(255))
-BEGIN
-    SET @query = CONCAT('USE ', dbName, ';');
-    PREPARE stmt FROM @query;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-
-    SET @query = '
-        CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
             id INT,
             name VARCHAR(256)
         );
-    ';
-    PREPARE stmt FROM @query;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-END //
-
-DELIMITER ;
+    
