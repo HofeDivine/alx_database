7f81fca04a1daf40ea22f0d81b95ed871df57514
HofeDivine <hofedive@gmail.com>
2024-01-20 20:15:25 -1200

    commit

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,4 @@
--- A script that lists all cities contained in the database hbtn_0d_usa.
-
-USE your_database_name;
+-- A script that lists all cities contained in the database hbtn_0d_usa
 
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
