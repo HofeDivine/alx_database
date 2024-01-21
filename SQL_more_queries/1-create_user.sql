eda11ee799a978966c717474387bd1a213fb763f
HofeDivine <hofedive@gmail.com>
2024-01-20 15:02:45 -1200

    COMMIT

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create user and grant all privilleges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 0N *.* TO 'user_0d_1'@ 'localhost';
+FLUSH PRIVILEGES;
