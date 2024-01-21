f55b416984e3c310f2122fd7f8ff9992d743ae44
HofeDivine <hofedive@gmail.com>
2024-01-20 13:10:11 -1200

    commit

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,12 @@
 -- Creating a second table 
-CREATE TABLE IF NOT EXISTS second_table(
-id INT
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
+name VARCHAR(256),
 score INT
 );
-INSERT INTO second_table
-VALUES(
-    (1,"John",10)
-    (2,"Alex",3)
-    (3,"Bob",14)
-    (4,"George",8)
-);
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1,"John",10),
+    (2,"Alex",3),
+    (3,"Bob",14),
+    (4,"George",8);
